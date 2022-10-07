d9d8d9af8aa3e8b86a469ac1c813a63107eebdd6
Shotaro Kumagai <75811152+shotarokuma@users.noreply.github.com>
2022-10-07 11:30:16 -0700

    add sample solution

--- 19.py
+++ 19.py
@@ -31,4 +31,17 @@ class Solution(object):
             head = last
         
         return head
+
+    def sample(self, head, n):
+        def index(node):
+          if not node:
+            return 0
+          i = index(node.next) + 1
+          if i > n:
+            node.next.val = node.val
+            return i
+          index(head)
+          return head.next
+
         
+
